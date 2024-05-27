6d259dac6fe82fa317986b0cf91d5c7013fae057
Edwalbert <monitoramento@cotramol.com.br>
2024-05-27 11:02:11 -0300

    Ajustes finais

--- main.js
+++ main.js
@@ -1,33 +1,41 @@
 document.addEventListener('DOMContentLoaded', function () {
+    const showPasswordEye = document.getElementById('show-password-eye');
+    const hidePasswordEye = document.getElementById('hide-password-eye');
     const closedEyesImg = document.getElementById('static-monkey-closed-eyes');
     const openEyesImg = document.getElementById('static-monkey-open-eyes');
     const gifImg = document.getElementById('gif-monkey');
-    const checkbox = document.getElementById('show-password');
     const passwordInput = document.getElementById('password');
 
-    openEyesImg.style.display = 'none'
-    gifImg.style.display = 'none'
+    openEyesImg.style.display = 'none';
+    gifImg.style.display = 'none';
 
-    checkbox.addEventListener('change', function (e) {
-        if (e.target.checked) {
-            //muda o campo senha para text
+    showPasswordEye.addEventListener('click', function () {
+        showOrHidePassword();
+    });
+
+    hidePasswordEye.addEventListener('click', function () {
+        showOrHidePassword();
+    });
+
+    function showOrHidePassword() {
+        if (passwordInput.type === 'password') {
+            showPasswordEye.style.display = 'none';
+            hidePasswordEye.style.display = 'block';
             passwordInput.type = 'text';
-            // Mostrar o GIF e esconder a imagem estática com olhos fechados
             closedEyesImg.style.display = 'none';
             gifImg.style.display = 'block';
 
-            // Após 1 segundo, esconder o GIF e mostrar a imagem estática com olhos abertos
             setTimeout(() => {
                 gifImg.style.display = 'none';
                 openEyesImg.style.display = 'block';
             }, 1000);
         } else {
-            // Voltar o campo de senha para o tipo password
+            hidePasswordEye.style.display = 'none';
+            showPasswordEye.style.display = 'block';
             passwordInput.type = 'password';
-            // Voltar para a imagem estática com olhos fechados
             openEyesImg.style.display = 'none';
             gifImg.style.display = 'none';
             closedEyesImg.style.display = 'block';
         }
-    });
+    }
 });
